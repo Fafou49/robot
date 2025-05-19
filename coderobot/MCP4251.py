3692d4562e9fb2f13795959737551d26cf490be3
Fafou49 <fabrice.greffier49@gmail.com>
2025-05-19 17:08:01 +0200

    cleaning the repo

--- coderobot/MCP4251.py
+++
@@ -1,1 +0,0 @@
-https://github.com/mholtrop/Phys605/blob/master/Python/DevLib/MCP4251.py
--- coderobot/Remote.py
+++ coderobot/Remote.py
@@ -1,137 +1,185 @@
-# Controller
 import evdev #le module evdev est expliqué ici: https://www.youtube.com/watch?v=2F4M-7IGlrc
 import numpy as np
 import pygame
 import time
+import gpiod
+import threading
 
-device = evdev.InputDevice('/dev/input/event5')
-print(device, " connected")
-pygame.init()
-pygame.joystick.init()
-WIDTH, HEIGHT = 1000, 680
-FPS = 60
-state_joystick = False
-
-
-# Vérifier si une manette est connectée
-try:
-    while True:
-        count = pygame.joystick.get_count()
-        if count!=0:
-            # Utiliser la première manette trouvée
-            state_joystick = True
-            joystick = pygame.joystick.Joystick(0)
-            joystick.init()
-
-            speed_x = 120
-            speed_y = 120
-            x1, y1 = WIDTH // 2, HEIGHT // 2
-            x2, y2 = WIDTH // 2, HEIGHT // 2
-            screen = pygame.display.set_mode((WIDTH, HEIGHT))
-            pygame.display.set_caption("Manettes")
-
-            sprite1 = pygame.Rect(x1, y1, 50, 50)
-            sprite2 = pygame.Rect(x2, y2, 50, 50)
-            timer = pygame.time.Clock()
-
-            running = True
-
-            while running:
-                for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        running = False
-                        pygame.quit()
-                if state_joystick:
-                    joystickGauche_x = joystick.get_axis(0)
-                    joystickGauche_y = joystick.get_axis(1)
-                    joystickDroit_x = joystick.get_axis(2)
-                    joystickDroit_y = joystick.get_axis(3)
-                    TriggerL = joystick.get_axis(4)
-                    TriggerR = joystick.get_axis(5)
-
-                    bouttonA = joystick.get_button(0)
-                    bouttonX = joystick.get_button(1)
-                    bouttonB = joystick.get_button(2)
-                    bouttonY = joystick.get_button(3)
-                    bouttonLT = joystick.get_button(4)
-                    bouttonRT = joystick.get_button(5)
-                    bouttonSelect = joystick.get_button(6)
-                    bouttonStart = joystick.get_button(7)
-                    bouttonLeftBumper = joystick.get_button(8)
-                    bouttonRightBumper = joystick.get_button(9)
-
-                    if joystickDroit_x >= 0.2:
-                        x1 += (speed_x / FPS)
-                    elif joystickDroit_x <= -0.2:
-                        x1 -= (speed_x / FPS)
-
-                    if joystickDroit_y >= 0.2:
-                        y1 += (speed_y / FPS)
-                    elif joystickDroit_y <= -0.2:
-                        y1 -= (speed_y / FPS)
-
-                    if joystickGauche_x >= 0.2:
-                        x2 += (speed_x / FPS)
-                    elif joystickGauche_x <= -0.2:
-                        x2 -= (speed_x / FPS)
-
-                    if joystickGauche_y >= 0.2:
-                        y2 += (speed_y / FPS)
-                    elif joystickGauche_y <= -0.2:
-                        y2 -= (speed_y / FPS)
-
-                    if x1 > WIDTH - 25:
-                        x1 = WIDTH - 25
-                    elif x1 < 0:
-                        x1 = 0
-                    if y1 > HEIGHT - 25:
-                        y1 = HEIGHT - 25
-                    elif y1 < 0:
-                        y1 = 0
-
-                    if x2 > WIDTH - 25:
-                        x2 = WIDTH - 25
-                    elif x2 < 0:
-                        x2 = 0
-                    if y2 > HEIGHT - 25:
-                        y2 = HEIGHT - 25
-                    elif y2 < 0:
-                        y2 = 0
-
-                    sprite1.x = x1
-                    sprite1.y = y1
-                    sprite2.x = x2
-                    sprite2.y = y2
-
-                    screen.fill((255, 255, 255))
-                    pygame.draw.rect(screen, pygame.Color("black"), sprite1)
-                    pygame.draw.rect(screen, pygame.Color("black"), sprite2)
-                    if bouttonA:
-                        pygame.draw.rect(screen, pygame.Color("green"), sprite1)
-                    if bouttonX:
-                        pygame.draw.rect(screen, pygame.Color("red"), sprite1)
-                    if bouttonB:
-                        pygame.draw.rect(screen, pygame.Color("blue"), sprite1)
-                    if bouttonY:
-                        pygame.draw.rect(screen, pygame.Color("yellow"), sprite1)
-                    if bouttonLT:
-                        pygame.draw.rect(screen, pygame.Color("orange"), sprite1)
-                    if bouttonRT:
-                        pygame.draw.rect(screen, pygame.Color("brown"), sprite1)
-                    if bouttonSelect:
-                        pygame.draw.rect(screen, pygame.Color("grey"), sprite1)
-                    if bouttonStart:
-                        pygame.draw.rect(screen, pygame.Color("pink"), sprite1)
+def pwm(PWMdutyCycleLeft,PWMdutyCycleRight):
+        while True:  
+                # 5.1 - Counting from 0 to 255 all the time  
+                for PWM_Counter in range(255):
+                        print ("dutyCycleRight : " , dutyCycleRight," / " ,"dutyCycleLeft : ",dutyCycleLeft )
+                        if dutyCycleLeft>20:
+                                if PWM_Counter>dutyCycleLeft:
+                                        M1_S1.set_value(0)
+                                else:
+                                        M1_S1.set_value(1)
+                        elif dutyCycleLeft<-20:
+                                if PWM_Counter>abs(dutyCycleLeft):
+                                        M1_S2.set_value(0)
+                                else:
+                                        M1_S2.set_value(1)
+                        else:
+                                M1_S1.set_value(0)
+                                M1_S2.set_value(0)
                         
-                    if bouttonLeftBumper:
-                        pygame.draw.rect(screen, pygame.Color(255, 0, 128), sprite2)
-                    if bouttonRightBumper:
-                        pygame.draw.rect(screen, pygame.Color(100, 100, 100), sprite2)
+                        if dutyCycleRight>20:
+                                if PWM_Counter>dutyCycleRight:
+                                        M2_S1.set_value(0)
+                                else:
+                                        M2_S1.set_value(1)
+                        elif dutyCycleRight<-20:
+                                if PWM_Counter>abs(dutyCycleRight):
+                                        M2_S2.set_value(0)
+                                else:
+                                        M2_S2.set_value(1)
+                        else:
+                                M2_S1.set_value(0)
+                                M2_S2.set_value(0)
+                
+                
+# 0 - variables definition
+chip = gpiod.Chip('gpiochip4')
+MOTOR1_SENS1 = 14
+MOTOR1_SENS2 = 15
+MOTOR2_SENS1 = 2
+MOTOR2_SENS2 = 3
+dutyCycleLeft=0
+dutyCycleRight=0
+device =0
+verrou = threading.Lock()
 
-                    print(joystickGauche_y, joystickDroit_y)
+# 1 - testing the remote connection 
+while device ==0:
+    try:
+        device = evdev.InputDevice('/dev/input/event5')
+    except:
+        print("No device connected")
+    else:
+        print(device, " connected")
+        pygame.init()
+        pygame.joystick.init()
+        FPS = 5
+        state_joystick = False
+    finally:
+        if device !=0:
+            print ("starting")
+        else:
+            print ("new try")
+            
+        # 2 - IF the remote is connected, THEN    
+        if device !=0:
+            # setting the OUTPUTS
+            M1_S1 = chip.get_line(MOTOR1_SENS1)
+            M1_S1.request(consumer="M1_S1", type=gpiod.LINE_REQ_DIR_OUT)
 
-                    pygame.display.update()
-                    timer.tick(FPS)
-finally:
-    time.sleep(5)
-    pygame.quit()
+            M1_S2 = chip.get_line(MOTOR1_SENS2)
+            M1_S2.request(consumer="M1_S2", type=gpiod.LINE_REQ_DIR_OUT)
+
+            M2_S1 = chip.get_line(MOTOR2_SENS1)
+            M2_S1.request(consumer="M2_S1", type=gpiod.LINE_REQ_DIR_OUT)
+
+            M2_S2 = chip.get_line(MOTOR2_SENS2)
+            M2_S2.request(consumer="M2_S2", type=gpiod.LINE_REQ_DIR_OUT)
+    
+            pwm_tread = threading.Thread(target=pwm, args=(dutyCycleLeft, dutyCycleRight,))
+            pwm_tread.start()                                
+                      
+    try:
+        while True:
+            try:
+                #Reading the remote status
+                count = pygame.joystick.get_count()
+            except KeyboardInterrupt:
+                M1_S1.set_value(0)
+                M2_S1.set_value(0)
+                M1_S2.set_value(0)
+                M2_S2.set_value(0)
+                pygame.quit() 
+            except:
+                print ("device disconected")
+                break
+            else:                
+                if count!=0:
+                    # using the remote found
+                    state_joystick = True
+                    joystick = pygame.joystick.Joystick(0)
+                    joystick.init()
+
+
+                timer = pygame.time.Clock()
+
+                running = True
+                # 4.1 - reading all the time the remote signals
+                while running:
+                    for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            running = False
+                            M1_S1.set_value(0)
+                            M2_S1.set_value(0)
+                            M1_S2.set_value(0)
+                            M2_S2.set_value(0)
+                            pygame.quit()
+                    if state_joystick:
+                        # 3 - pushing the remote signals to understandable variables
+                        joystickGauche_x = joystick.get_axis(0)
+                        joystickGauche_y = joystick.get_axis(1)
+                        joystickDroit_x = joystick.get_axis(2)
+                        joystickDroit_y = joystick.get_axis(3)
+                        TriggerL = joystick.get_axis(4)
+                        TriggerR = joystick.get_axis(5)
+
+                        bouttonA = joystick.get_button(0)
+                        #print("bouttonA 0 :",bouttonA)
+                        bouttonB = joystick.get_button(1)
+                        #print("bouttonB 1 :",bouttonB)
+                        bouttonX = joystick.get_button(3)
+                        #print("bouttonX 3 :",bouttonX)
+                        bouttonY = joystick.get_button(4)
+                        #print("bouttonY 4 :",bouttonY)
+                        bouttonLT = joystick.get_button(6)
+                        #print("bouttonLT 6 :",bouttonLT)
+                        bouttonRT = joystick.get_button(7)
+                        #print("bouttonRT 7 :",bouttonRT)
+                        bouttonSelect = joystick.get_button(10)
+                        #print("bouttonSelect 10 :",bouttonSelect)
+                        bouttonStart = joystick.get_button(11)
+                        #print("bouttonStart 11 :",bouttonStart)
+                        bouttonLeftBumper = joystick.get_button(13)
+                        #print("bouttonLeftBumper 13 :",bouttonLeftBumper)
+                        bouttonRightBumper = joystick.get_button(14)
+                        #print("bouttonRightBumper 14 :",bouttonRightBumper)
+                        
+                        #finish the loop if signal "START"
+                        if bouttonStart==1:
+                            M1_S1.set_value(0)
+                            M2_S1.set_value(0)
+                            M1_S2.set_value(0)
+                            M2_S2.set_value(0)
+                            running = False
+                            pygame.quit()
+                            break
+                            
+                        with verrou:
+                        # 4.2 Converting the Joysticks signals to the Motor values (-255 to 255)
+                                dutyCycleRight = int((-joystickDroit_y) *255)
+                                dutyCycleLeft = int((-joystickGauche_y )*255)
+                         
+                          
+                        #wait the FPS time
+                        timer.tick(FPS)
+
+    # closing the process and turnning of the MOTORS
+    finally:
+        print("end!!!")
+        M1_S1.set_value(0)
+        M2_S1.set_value(0)
+        M1_S2.set_value(0)
+        M2_S2.set_value(0)
+        try:
+            pygame.quit() 
+        except:
+            print("No device connected")
+        finally:
+            print("Good Bye")
